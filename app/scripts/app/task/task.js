a506d786db7f4eddfad8e3a35045b1a5d92ae496
zengping <331962716@qq.com>
2016-11-28 10:02:20 +0800

    modify

--- app/scripts/app/task/task.js
+++ app/scripts/app/task/task.js
@@ -37,28 +37,18 @@ define(['../jLibs'], function(jLibs) {
     function loadEvent() {
         var button = document.querySelector("button");
         button.onclick = function(event) {
-            location.href = "";
+            location.href = "#";
             event.stopPropagation();
         };
+
+        jLibs.loadComponent("menu/menu");
     }
 
     (function getTmpl() {
         t.page = `
             <div id="task">
-                <ul>
-                    <li class="" j-for="v in data">
-                        <a class="sys-sidebar-item" href="javascript:void(0);">{{v.name}}</a>
-                        <ul class="sys-sidebar-child">
-                            <li class="" j-for="(key,val) in v.child" data-url="{{val.url}}">
-                                <a href="#{{val.url}}">
-                                    <i class="{{val.icon}}"></i>
-                                    {{val.name}}
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
             </div>
+            <menu></menu>
 
             <button>to menu</button>
         `;
